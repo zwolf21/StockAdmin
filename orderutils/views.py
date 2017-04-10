be6cb4a0d563deff5947b010f9933a2c61f4b5b7
zwolf21 <pbr112@naver.com>
2017-04-11 00:08:25 +0900

    ok

--- orderutils/views.py
+++ orderutils/views.py
@@ -46,7 +46,7 @@ class LabelCollectLV(ListView):
 			return []
 		ward = ward.split(", ")
 
-		queryset = get_label_object_test(['S','P'], ward, ord_start_date, ord_end_date, start_time, end_time)
+		queryset = get_label_object(['S','P'], ward, ord_start_date, ord_end_date, start_time, end_time)
 		return queryset
 
 
